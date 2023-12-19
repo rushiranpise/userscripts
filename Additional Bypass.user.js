87a1d85af897b85835216dc6af639abc8ae62a11
Rushi Ranpise <rushiranpise17@gmail.com>
2023-12-19 16:24:43 +0000

    Updated Scripts on 12/19/23

--- Additional Bypass.user.js
+++
@@ -1,1 +0,0 @@
-<html><body>You are being <a href="https://update.greasyfork.org/scripts/443888/Additional%20Bypass.user.js">redirected</a>.</body></html>
--- Bypass All Shortlinks.user.js
+++
@@ -1,1 +0,0 @@
-<html><body>You are being <a href="https://update.greasyfork.org/scripts/431691/Bypass%20All%20Shortlinks.user.js">redirected</a>.</body></html>

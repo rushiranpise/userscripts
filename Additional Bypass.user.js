6f37a5dce2cdb11df0a3ab842f4554db33e51901
Rushi Ranpise <rushiranpise17@gmail.com>
2024-01-05 16:27:19 +0000

    Updated Scripts on 01/05/24

---
+++ Additional Bypass.user.js
@@ -0,0 +1,9 @@
+<html>
+<head><title>502 Bad Gateway</title></head>
+// @updateURL https://github.com/rushiranpise/userscripts/raw/main/Additional%20Bypass.user.js
+// @downloadURL https://github.com/rushiranpise/userscripts/raw/main/Additional%20Bypass.user.js
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+<hr><center>nginx/1.18.0 (Ubuntu)</center>
+</body>
+</html>
